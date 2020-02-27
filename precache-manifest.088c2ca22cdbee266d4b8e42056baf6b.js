1f6f7fe721db3b086bce81b5e943943cdd83bdf1
SamathaAleti <61436254+SamathaAleti@users.noreply.github.com>
2020-02-27 12:31:42 -0600

    Updates

---
+++ precache-manifest.088c2ca22cdbee266d4b8e42056baf6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3551132b9e5f9685214e15db2ee5158c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6fc32b3a835039cfb59f",
+    "url": "/monsters-rolodex/static/css/main.0d80684f.chunk.css"
+  },
+  {
+    "revision": "8c39acc1117c8474bddd",
+    "url": "/monsters-rolodex/static/js/2.f035c78a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.f035c78a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fc32b3a835039cfb59f",
+    "url": "/monsters-rolodex/static/js/main.1caf9baf.chunk.js"
+  },
+  {
+    "revision": "24034436bd08b53a5646",
+    "url": "/monsters-rolodex/static/js/runtime-main.6386504b.js"
+  }
+]);
